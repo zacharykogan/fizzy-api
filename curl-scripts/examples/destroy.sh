64dcd31e5b7287a662955668abdcf40f26424b81
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-08-05 12:32:48 -0400

    updated models

--- curl-scripts/examples/destroy.sh
+++ curl-scripts/examples/destroy.sh
@@ -9,3 +9,10 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Authorization: Bearer ${TOKEN}"
 
 echo
+
+
+# //
+# Request:
+
+# ```sh
+# curl --include --request DELETE http://localhost:4741/sign-out/ \
