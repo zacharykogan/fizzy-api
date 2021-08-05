f8ebce39153244c38f2c9b1c17e7b6f248f29ba8
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-08-05 19:24:06 -0400

    finished all the routes for user and reviews

--- app/models/review.js
+++ app/models/review.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose')
 
 const reviewSchema = new mongoose.Schema({
-  drink: {
-    name: String,
+  name: {
     type: String,
-    review: String,
-    rating: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 5
-    }},
+    required: true
+  },
+  review: { 
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   }, 
-  timestamps: true
+
   })
 
 module.exports = mongoose.model('Review', reviewSchema)
