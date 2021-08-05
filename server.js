8a1676d01313db87e9a7c403d45e6216dfab52e9
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-08-05 11:23:54 -0400

    Updated server.js with review routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const reviewRoutes = require('./app/routes/review_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(reviewRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
